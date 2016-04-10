7c43f118c36a9ef008d51b78a1453b13d686566f
smiller657 <smiller3@mail.umw.edu>
2016-04-10 18:34:14 -0400

    Merge origin/newsfeed1 into newsfeed1
    Conflicts:
    	TwitterBlackout/src/Hastag.java
    	TwitterBlackout/src/TwitterBlackoutTester.java

--- TwitterBlackout/src/TwitterBlackoutTester.java
+++ TwitterBlackout/src/TwitterBlackoutTester.java
@@ -16,12 +16,26 @@ public class TwitterBlackoutTester {
         ArrayList<Subscription> subs = new ArrayList<Subscription>();
         ArrayList<Hashtag> hashtags = new ArrayList<Hashtag>();
         User sm = new User(0, "Sam", "Miller", "swordgirl", "password", true);
+        User pk = new User(1, "Paul", "Kartage", "rennman", "pass2", false);
         User mr = new User(1, "Mike", "Reynolds", "mrmr", "1234", true);
-        /* Tweet sm1 = new Tweet(0, 0, "sam's tweet 1", true, "160101");
+        /* Testing code tools before db implementation:
+        
+        users.add(sm);
+        users.add(pk);
+        Tweet sm1 = new Tweet(0, 0, "sam's tweet 1", true, "160101");
         Tweet sm2 = new Tweet(1, 0, "sam's tweet 2", true, "160102");
         Tweets tweets = new Tweets();
         tweets.addTweet(sm1);
-        tweets.addTweet(sm2); */
+        tweets.addTweet(sm2); 
+        Tweet pk1 = new Tweet(2, 1, "Paul's private tweet #1", false, "160302");
+        Tweet pk2 = new Tweet(3, 1, "Paul's public tweet #2", true, "160202");
+        tweets.add(pk1);
+        tweets.add(pk2);
+        Subscription sub1 = new Subscription(100, 1, 0);
+        subs.add(sub1);
+        TwitterBlackout tb = new TwitterBlackout();
+        tb.runApp(tweets, users, subs, hashtags);
+        */
         
         // test connection
         /* try {
@@ -45,25 +59,7 @@ public class TwitterBlackoutTester {
         else
             iP = 0; */
         User var = mr;
-            
         Database.addUser(var);
-        
-        
-        User pk = new User(1, "Paul", "Kartage", "rennman", "pass2", false);
-        users.add(sm);
-        users.add(pk);
-        Tweet sm1 = new Tweet(0, 0, "sam's public tweet 1", true, "160101");
-        Tweet sm2 = new Tweet(1, 0, "sam's private tweet 2", false, "160102");
-        tweets.add(sm1);
-        tweets.add(sm2);
-        Tweet pk1 = new Tweet(2, 1, "Paul's private tweet #1", false, "160302");
-        Tweet pk2 = new Tweet(3, 1, "Paul's public tweet #2", true, "160202");
-        tweets.add(pk1);
-        tweets.add(pk2);
-        Subscription sub1 = new Subscription(100, 1, 0);
-        subs.add(sub1);
-        TwitterBlackout tb = new TwitterBlackout();
-        tb.runApp(tweets, users, subs, hashtags);
     }
     
 }
